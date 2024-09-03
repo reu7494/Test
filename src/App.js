83d2647efbc03d0e9b825eb316798e6b63177d1c
black-ne-ko <reu7494@gmail.com>
2024-09-03 23:37:58 +0900

    ToDoList

--- src/App.js
+++ src/App.js
@@ -2,40 +2,46 @@ import { useState } from "react";
 
 let nextList = [
   { id: 0, name: "세탁기 작동시키기" },
-  { id: 1, name: "배추 구매하기" }
+  { id: 1, name: "배추 구매하기" },
 ];
 
 let nextId = 2;
-if(nextList.id === 0){
+if (nextList.id === 0) {
   nextId = 1;
-}else if(nextList.id === 1){
+} else if (nextList.id === 1) {
   nextId = 0;
 }
 
 export default function List() {
   const [name, setName] = useState();
-  const [lists, setLists] = useState(nextList, []);
+  const [lists, setLists] = useState(nextList);
+
+  function handleAdd() {
+    setName("");
+    setLists([...lists, { id: nextId++, name: name }]);
+  }
   return (
     <>
       <h1>To Do List</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button
-        onClick={() => {
-          setName('');
-          setLists([...lists, { id: nextId++, name: name }]);
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleAdd();
+          }
         }}
-      >
-        Add
-      </button>
+      />
+      <button onClick={handleAdd}>Add</button>
       <ul>
         {lists.map((list) => (
           <li key={list.id}>
             {list.name}{" "}
             <button
-                onClick={() => {
-                  setLists(lists.filter((a) => a.id !== list.id));
-                }}
-           >
+              onClick={() => {
+                setLists(lists.filter((a) => a.id !== list.id));
+              }}
+            >
               Delete
             </button>
           </li>
