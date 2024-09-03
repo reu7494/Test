14c1efbfbacb095ff787fafbc2b8da3a8b400355
reu7494 <61904142+reu7494@users.noreply.github.com>
2024-09-03 14:26:19 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,43 @@ import { useState } from "react";
 
 let nextList = [
   { id: 0, name: "세탁기 작동시키기" },
-  { id: 1, name: "배추 구매하기" },
+  { id: 1, name: "배추 구매하기" }
 ];
 
 let nextId = 2;
+if(nextList.id === 0){
+  nextId = 1;
+}else if(nextList.id === 1){
+  nextId = 0;
+}
 
 export default function List() {
   const [name, setName] = useState();
-  const [list, setList] = useState(nextList, []);
+  const [lists, setLists] = useState(nextList, []);
   return (
     <>
       <h1>To Do List</h1>
       <input value={name} onChange={(e) => setName(e.target.value)} />
       <button
         onClick={() => {
-          setList([...list, { id: nextId++, name: name }]);
+          setName('');
+          setLists([...lists, { id: nextId++, name: name }]);
         }}
       >
         Add
       </button>
       <ul>
-        {list.map((list) => (
-          <li key={list.id}>{list.name}</li>
+        {lists.map((list) => (
+          <li key={list.id}>
+            {list.name}{" "}
+            <button
+                onClick={() => {
+                  setLists(lists.filter((a) => a.id !== list.id));
+                }}
+           >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
