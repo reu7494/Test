6f4fd2e2d8ecc609ee4beba215144ee58a17757f
black-ne-ko <reu7494@gmail.com>
2024-11-11 01:14:28 +0900

    ToDoList

--- server/listServer.js
+++ server/listServer.js
@@ -17,10 +17,10 @@ app.use(express.json());
 
 // MySQL 데이터베이스 설정
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "1234",
+  database: process.env.DB_NAME || "todolistdatabase",
 });
 
 // MySQL 연결
